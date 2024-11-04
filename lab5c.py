7eed2bbd7920f95b3486df23b180c4eb45cecf3b
vdesai19-seneca <156854849+vdesai19-seneca@users.noreply.github.com>
2024-11-04 10:58:46 -0500

    lab5c

---
+++ lab5c.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Author ID: vdesai19
+
+def add(number1, number2):
+    # Add two numbers together, return the result, if error return string 'error: could not add numbers'
+    try:
+        return int(number1) + int(number2)
+    except ValueError:
+        return 'error: could not add numbers'
+
+
+def read_file(filename):
+    # Read a file, return a list of all lines, if error return string 'error: could not read file'
+    try:
+        with open(filename, 'r') as file:
+           return file.readlines()
+    except IOError:
+        return 'error: could not read file'
+
+if __name__ == '__main__':
+    print(add(10,5))                        # works
+    print(add('10',5))                      # works
+    print(add('abc',5))                     # exception
+    print(read_file('seneca2.txt'))         # works
+    print(read_file('file10000.txt'))       # exception
